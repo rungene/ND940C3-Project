b39dbaf7685636bcd6ab2fde34afe7fc193546fa
rungene <rungenesamuel@gmail.com>
2021-03-23 19:09:39 +0300

    updating DetailsActivity and the Layout

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,15 +1,35 @@
 package com.udacity
 
+import android.app.NotificationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var notificationManager: NotificationManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        //intialize notification manager
+        notificationManager =ContextCompat.getSystemService(
+                applicationContext,NotificationManager::class.java
+        )as NotificationManager
+
+        notificationManager.cancelAll()
+
+        val nameFile=intent.getStringExtra("fileName")
+        val detailsStatus =intent.getStringExtra("status")
+
+        file_name.text=nameFile
+        status.text =detailsStatus
+
+
+
     }
 
 }
